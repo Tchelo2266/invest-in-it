afe166a3b3ad620fbd095a14d47b50e0bc87b6ad
Marcelo <tchelo2266@outlook.com>
2022-03-30 18:43:31 -0300

    Arrumando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './views/home-page/home-page.component';
 import { LoginComponent } from './views/login/login.component';
+import { PerfilComponent } from './views/perfil/perfil.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'perfil', component: PerfilComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './views/home-page/home-page.component';
 import { LoginComponent } from './views/login/login.component';
+import { PerfilComponent } from './views/perfil/perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     LoginComponent,
+    PerfilComponent,
   ],
   imports: [
     BrowserModule,
