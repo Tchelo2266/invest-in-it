5a11283b71a976d0d32d8adba12d87aff7522069
Marcelo <tchelo2266@outlook.com>
2022-04-13 19:18:52 -0300

    Nem sei q q eu fiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './views/home-page/home-page.component';
 import { LoginComponent } from './views/login/login.component';
 import { PerfilComponent } from './views/perfil/perfil.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-// import { IconsModule } from './icons/icons.module';
+
 
 @NgModule({
   declarations: [
@@ -15,13 +15,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HomePageComponent,
     LoginComponent,
     PerfilComponent,
-    // FontAwesomeModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule
-    // IconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
